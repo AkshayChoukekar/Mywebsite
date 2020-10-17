fbd78f2945fccb9e4fb95972645b4606cf187d37
akshay choukekar <akshay.choukekar007@gmail.com>
2020-10-17 12:51:01 +0530

    first draft of website

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".btn").click(function(){
+      $(".header").slideUp();
+    });   
+    $(".home-btn").click(function(){
+      $(".header").slideDown();
+    });
+});
