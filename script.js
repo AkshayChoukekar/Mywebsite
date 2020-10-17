fcae2ec322cafdeef0d03a5510839971493db0fb
Akshay Choukekar <55825139+AkshayChoukekar@users.noreply.github.com>
2020-10-17 12:35:00 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".btn").click(function(){
-      $(".header").slideUp();
-    });   
-    $(".home-btn").click(function(){
-      $(".header").slideDown();
-    });
-});
